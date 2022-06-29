25d5fcccb9e1bbe5477773b67c143e804bf8771a
NickAstley <andreanicastro1994@gmail.com>
2022-06-29 17:57:56 +0200

    Completed Milestone 3

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,19 +1,19 @@
 <template>
     <ul>
         <li>
-            <img :src="searchItemPoster" :alt="`Poster di ${searchItemTitle}`">
+            <img :src="itemPoster" :alt="`Poster di ${itemTitle}`">
         </li>
         <li>
-            <b>Titolo: </b> {{ searchItemTitle }}
+            <b>Titolo: </b> {{ itemTitle }}
         </li>
         <li>
-            <b>Titolo originale: </b> {{ searchItemOriginalTitle }}
+            <b>Titolo originale: </b> {{ itemOriginalTitle }}
         </li>
         <li>
-            <b>Lingua: </b> <span class="fi" :class="`fi-` + countryFlag"></span>
+            <b>Lingua: </b> <span class="fi" :class="`fi-` + itemCountryFlag"></span>
         </li>
         <li>
-            <b>Voto: </b> {{ searchItem.vote_average }}
+            <b>Voto: </b> <i v-for="i in itemAverageVote[0]" :key="i" class="fa-solid fa-star"></i><i v-if="itemAverageVote[1]" class="fa-solid fa-star-half"></i>
         </li>
     </ul>
 </template>
@@ -25,25 +25,25 @@
         },
 
         computed: {
-            searchItemPoster() {
+            itemPoster() {
                 if (this.searchItem.poster_path === null) {
                     return require("../assets/placeholder-w342.png");
                 }
                 return `https://image.tmdb.org/t/p/w342${this.searchItem.poster_path}`;
             },
-            searchItemTitle() {
+            itemTitle() {
                 if(this.searchItem.title) {
                     return this.searchItem.title;
                 }
                 return this.searchItem.name;
             },
-            searchItemOriginalTitle() {
+            itemOriginalTitle() {
                 if(this.searchItem.original_title) {
                     return this.searchItem.original_title;
                 }
                 return this.searchItem.original_name;
             },
-            countryFlag() {
+            itemCountryFlag() {
                 const languageMap = {
                     "da" : "dk",
                     "en" : "us",
@@ -57,11 +57,23 @@
                     return languageMap[this.searchItem.original_language];
                 }
                 return this.searchItem.original_language;
+            },
+            itemAverageVote() {
+                const halfVote = this.searchItem.vote_average / 2;
+                if(halfVote % 1 >= 0.5) {
+                    return [Math.floor(halfVote), 1]
+                }
+                return [Math.floor(halfVote)];
             }
         }
     }
 </script>
 
-<style>
+<style lang="scss">
     @import "~flag-icons/css/flag-icons.css";
+    @import "~@fortawesome/fontawesome-free/css/all.min.css";
+
+    .fa-star, .fa-star-half {
+        color: #FEBC00;
+    }
 </style>
